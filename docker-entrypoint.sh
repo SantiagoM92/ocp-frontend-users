5476429c9b3042d219ea367426b2108907701f95
jmartinez <jairo.martinez@fisagrp.com>
2025-11-10 23:59:20 -0500

    fix: reverse proxy

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,6 +12,8 @@ export NGINX_LISTEN_PORT
 
 TMP_CONF_DIR="${TMP_CONF_DIR:-/tmp/nginx}"
 mkdir -p "${TMP_CONF_DIR}"
+mkdir -p "${TMP_CONF_DIR}/client_temp" "${TMP_CONF_DIR}/proxy_temp" \
+  "${TMP_CONF_DIR}/fastcgi_temp" "${TMP_CONF_DIR}/uwsgi_temp" "${TMP_CONF_DIR}/scgi_temp"
 
 TEMPLATE_SRC="${NGINX_TEMPLATE_PATH:-/etc/nginx/conf.d/default.conf.template}"
 TEMPLATE_DEST="${TMP_CONF_DIR}/nginx.conf"
